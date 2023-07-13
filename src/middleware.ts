84c0d88f6314448f7476fdf19e0f1478aa4d6ff9
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-07-13 20:50:11 +0800

    Router

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({publicRoutes: ["/"]});
+export default authMiddleware({publicRoutes: ["/", "/api/trpc/posts.getAll"]});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
